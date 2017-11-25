0d18d5ea4e6ac2f4cf4119d27a4c5cb99c790227
Franz Benthin <franz.benthin@fahrradjaeger.de>
2017-11-25 12:58:51 +0100

    add vector icons, add bitter font, separate toggle layout, launcher activity

---
+++ app/src/main/java/pu/zajhhaptaueuh/ztanphsop/BikeDetailActivity.kt
@@ -0,0 +1,16 @@
+package pu.zajhhaptaueuh.ztanphsop
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+* ©Franz Benthin
+*/
+class BikeDetailActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bike_detail)
+    }
+
+}
