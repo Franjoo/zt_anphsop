8c79ef0a94a5e2e06ee4871183740b68a26d2739
franz <franz.benthin@fahrradjaeger.de>
2017-12-09 18:14:35 +0100

    edit bike prototype, styles, user choices, strings

--- app/src/main/java/pu/zajhhaptaueuh/ztanphsop/usecases/bikedetail/EditBikeActivity.kt
+++ app/src/main/java/pu/zajhhaptaueuh/ztanphsop/usecases/bikedetail/EditBikeActivity.kt
@@ -1,6 +1,7 @@
 package pu.zajhhaptaueuh.ztanphsop.usecases.bikedetail
 
 import android.os.Bundle
+import android.widget.NumberPicker
 import pu.zajhhaptaueuh.ztanphsop.R
 import pu.zajhhaptaueuh.ztanphsop.usecases.BaseActivity
 
@@ -13,8 +14,30 @@ class EditBikeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.bike_edit_bike)
         setupActionBar(null)
+//        setupPickerValues()
+    }
+
+    private fun setupPickerValues() {
+//        val pickerColorPrimary = findViewById<NumberPicker>(R.id.input_picker_primary_color)
+//        val pickerColorSecondary = findViewById<NumberPicker>(R.id.input_picker_secondary_color)
+//
+//        val values = arrayOf("Red", "Green", "Blue", "Yellow", "Magenta")
+//
+//        pickerColorPrimary.displayedValues = values
+//        pickerColorSecondary.displayedValues = values
+//
+//        pickerColorPrimary.minValue = 0
+//        pickerColorSecondary.minValue = 0
+//
+//        pickerColorPrimary.maxValue = values.size - 1
+//        pickerColorSecondary.minValue = values.size -1
+//
+//        pickerColorPrimary.wrapSelectorWheel = true
+//        pickerColorSecondary.wrapSelectorWheel = true
+
     }
 
 }
